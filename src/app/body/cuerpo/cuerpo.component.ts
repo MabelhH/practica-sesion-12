c2a992a06b8c9c88e53cace11ce53a0ad4ea339d
clara23 <clarahuanaco20@gmail.com>
2023-01-05 11:55:55 -0500

    actualisacion nueva

--- src/app/body/cuerpo/cuerpo.component.ts
+++ src/app/body/cuerpo/cuerpo.component.ts
@@ -7,15 +7,30 @@ import { Component } from '@angular/core';
 })
 export class CuerpoComponent {
  mostrar:boolean=false
- mensage:string='';
+ mensage:string='cel:937375934,Email:clarhunaco23@gmail.com,Locacion:Arequipa-PERU';
  mensage_contacto:string='mostrar';
 
  mostrarocultar(){
   if(this.mostrar){
     this.mostrar=false;
-    this.mensage_contacto='mostrar';
+    this.mensage_contacto='click para mostrar datos';
   }else{
-    
+    this.mostrar=true;
+    this.mensage_contacto='contactos';
+
   }
  }
+ public valores:string[]=[
+  'honestidad','justicia','empatia','creatividad','constancia'
+  
+]
+
+public valores2:string[]=[
+  
+    'tranparencia','libertad','puntualidad','adaptabilidad',
+  
+]
+
+
+
 }
